13f731b65af16f5e89ba8dc2ac0b71093c052028
diezul <eucodrut69@gmail.com>
2025-02-04 15:58:33 +0200

    Ensure pages folder is removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-900 text-white">{children}</body>
+      <body className="bg-gray-900 text-white font-sans">{children}</body>
     </html>
   );
 }
